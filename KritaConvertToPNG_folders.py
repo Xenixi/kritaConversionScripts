5832ee139b9c0ea5adac92adfece67a7686eefca
Kobe McManus <kobe.mcmanus@xenixi.com>
2022-01-17 15:54:47 -0500

    added scripts for bmp, tif, png

---
+++ KritaConvertToPNG_folders.py
@@ -0,0 +1,10 @@
+from distutils import filelist
+import glob
+import os
+import string
+
+for filename in glob.glob("*.kra"):
+    os.makedirs(filename[:-4])
+    os.system("krita " + filename + " --export --export-filename " + filename[:-4] + "/" + filename[:-4] + ".png")    
+    os.rename(filename, filename[:-4] + "/" + filename)
+    print("KritaConvertToPSD_folders: Converted: " + filename)
---
+++ KritaConvertToTIF.py
@@ -0,0 +1,12 @@
+from distutils import filelist
+import glob
+import os
+import string
+import uuid
+tempID = str(uuid.uuid4())
+os.makedirs("Finalized_" + tempID)
+for filename in glob.glob("*.kra"):
+   
+    os.system("krita " + filename + " --export --export-filename " + "Finalized_" + tempID + "/" + filename[:-4] + ".tif")    
+    os.rename(filename,"Finalized_" + tempID + "/" + filename)
+    print("KritaConvertToPSD: Converted: " + filename)
